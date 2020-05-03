7e90b7420aad4df6554e245f30f7ebbecc6b6385
Brian Gladman <brg@gladman.plus.com>
2020-05-03 18:30:14 +0100

    add tests to the build

---
+++ build.vs/build_tests/build_tests.py
@@ -0,0 +1,150 @@
+'''
+Set up Visual Sudio to build a specified MPIR configuration
+
+Copyright (C) 2011, 2012, 2013, 2014, 2015 Brian Gladman
+'''
+
+from operator import itemgetter
+from os import listdir, walk, unlink, makedirs
+from os.path import split, splitext, isdir, relpath, join, exists
+from os.path import dirname, normpath
+from copy import deepcopy
+from sys import argv, exit
+from filecmp import cmp
+from shutil import copy
+from re import compile, search
+from collections import defaultdict
+from uuid import uuid4
+from time import sleep
+
+from _msvccompiler import MSVCCompiler
+
+# for script debugging
+debug = False
+
+# add user choice
+flib_type = 'single' # ('gc', 'reentrant', 'single')
+
+# The path to flint, solution and project directories
+script_dir = dirname(__file__)
+project_name = 'pthreads'
+build_vc = 'build.vs19'
+pthreads_dir = normpath(join(script_dir, '../../tests'))
+solution_dir = normpath(join(pthreads_dir, 'build.vs'))
+
+app_type, lib_type, dll_type = 0, 1, 2
+app_str = ('Application', 'StaticLibrary', 'DynamicLibrary')
+app_ext = ('.exe', '.lib', '.dll')
+
+# copy from file ipath to file opath but avoid copying if
+# opath exists and is the same as ipath (this is to avoid
+# triggering an unecessary rebuild).
+
+def write_f(ipath, opath):
+  if exists(ipath) and not isdir(ipath):
+    if exists(opath):
+      if isdir(opath) or cmp(ipath, opath):
+        return
+    copy(ipath, opath)
+
+ignore_dirs = ( '.git', 'doc', 'examples', 'lib', 'exe', 'dll', 'win_hdrs')
+req_extns = ( '.h', '.c', '.cc', '.cpp' )
+
+def find_src(path):
+
+  c, h, cx, hx, t, tx, p = [], [], [], [], [], [], []
+  for root, dirs, files in walk(path):
+    if 'template' in root:
+      continue
+    _, _t = split(root)
+    if _t in ignore_dirs:
+      continue
+    if 'build.vc' in root:
+      for di in list(dirs):
+        dirs.remove(di)
+    for di in list(dirs):
+      if di in ignore_dirs:
+        dirs.remove(di)
+      if 'template' in di:
+        dirs.remove(di)
+    relp = relpath(root, pthreads_dir)
+    if relp == '.':
+      relp = ''
+    for f in files:
+      if 'template' in f:
+        continue
+      n, x = splitext(f)
+      if x not in req_extns:
+        continue
+      pth, leaf = split(root)
+      fp = join(relp, f)
+      if leaf == 'tune':
+        continue
+      if leaf == 'test':
+        p2, l2 = split(pth)
+        l2 = '' if l2 == 'flint2' else l2
+        if 'flintxx' in pth:
+          tx += [(l2, fp)]
+        else:
+          t += [(l2, fp)]
+      elif leaf == 'profile':
+        p2, l2 = split(pth)
+        l2 = '' if l2 == 'flint2' else l2
+        p += [(l2, fp)]
+      elif leaf == 'flintxx':
+        cx += [fp]
+      elif x == '.c':
+        c += [(leaf, fp)]
+      elif x == '.h':
+        if n.endswith('xx'):
+          hx += [fp]
+        else:
+          h += [fp]
+  for x in (c, h, cx, hx, t, tx, p):
+    x.sort()
+  return (c, h, cx, hx, t, tx, p)
+
+c, h, cx, hx, t, tx, p = find_src(pthreads_dir)
+
+# def compile(self, sources,
+#       output_dir=None, macros=None, include_dirs=None, debug=0,
+#       extra_preargs=None, extra_postargs=None, depends=None):
+
+# def link(self, target_desc, objects, output_filename, output_dir=None,
+#       libraries=None, library_dirs=None, runtime_library_dirs=None,
+#       export_symbols=None, debug=0, extra_preargs=None,
+#       extra_postargs=None, build_temp=None, target_lang=None):
+
+# def create_static_lib(self, objects, output_libname, output_dir=None,
+#       debug=0, target_lang=None):
+
+intd = '\\x64\\Release\\'
+
+cc = MSVCCompiler()
+error_list = []
+for l2, fp in c:
+  fdn, fx = splitext(fp)
+  fd, fn = split(fdn)
+  source = [join('..\\..\\tests', fp)]
+  inc_dirs = [
+    '..\\..\\'
+    ]
+  defines = [
+    '/D PTW32_STATIC_LIB',
+    '/D HAVE_PTW32_CONFIG_H',
+    '/FI ..\..\config.h'
+    ]
+  libs = [
+    '..\\..\\lib' + intd + 'pthreads'
+  ]
+  tmp_dir =  '.\\tests'
+  outd = '.\\tests'  + intd
+  try:
+    obj = cc.compile(source, output_dir=tmp_dir, defines=defines, include_dirs=inc_dirs)
+    cc.link("executable", obj, fn + '.exe', output_dir=outd, libraries=libs)
+  except:
+    error_list += [(l2, fp)]
+
+print('Build Errors:')
+for l2, fp in error_list:
+  print('    ', l2, fp)
---
+++ build.vs/run_tests/run_tests.py
@@ -0,0 +1,119 @@
+﻿
+from __future__ import print_function
+
+from os import chdir, walk
+from os.path import join, dirname, normpath, split, splitext
+import shutil
+import string
+import copy
+import subprocess
+import code
+import sys
+import re
+from time import sleep
+
+# timeout for running each test
+test_time_limit = 60
+
+vs_version = 19
+if len(sys.argv) > 1:
+  vs_version = int(sys.argv[1])
+
+build_dir_name = 'build.vs'
+
+script_dir = dirname(__file__)
+chdir(script_dir)
+t = join(script_dir, '..\\..\\' + build_dir_name)
+exe_dir = normpath(join(t, 'build_tests'))
+
+def split_pnx(p):
+  h, t = split(p)
+  n, x = splitext(t)
+  if x == '.filters':
+    n, _ = splitext(x)
+  return (h, n, x)
+
+exe = []
+for root, dirs, files in walk(exe_dir, topdown=False):
+  for x in files:
+    h, t = splitext(x)
+    if t == '.exe':
+      exe.append(join(root, x))
+
+build_fail = 0
+run_ok = 0
+run_fail = 0
+timed_out_fail = 0
+print(len(exe))
+
+fails, timed_out = [], []
+for ef in exe:
+  fdn, fx = splitext(ef)
+  fd, fn = split(fdn)
+  fd = fd[fd.find('tests') + 6 : fd.find('\\x64\\')]
+  fd = fd + ': ' + fn
+  print(fd)
+  try:
+    prc = subprocess.Popen( ef, stdout = subprocess.PIPE,
+      stderr = subprocess.STDOUT, creationflags = 0x08000000 )
+  except Exception as str:
+    print(fd, ' ... ERROR (', str, ')')
+    run_fail += 1
+    continue
+  for _ in range(test_time_limit):
+    sleep(1)
+    if prc.poll() is not None:
+      output = prc.communicate()[0]
+      break
+  else:
+    print(fd + '... ERROR (test timed out)')
+    prc.kill()
+    timed_out_fail += 1
+    timed_out += [fd]
+    continue
+  if output:
+    op = output.decode().replace('\n', '')
+  if prc.returncode:
+    t = fd + ' ...ERROR {}'.format(prc.returncode)
+    print(t, end=' ')
+    fails += [t + ' ' + op]
+    run_fail += 1
+  else:
+    run_ok += 1
+  if output:
+    op = output.decode().replace('\n', '')
+    if 'PASS' in op:
+      print(fd + ' ... PASS')
+    elif 'SKIPPED' in op:
+      print(fd + ' ... SKIPPED')
+    else:
+      print('output from ' + op)
+  else:
+    print()
+#  else:
+#    print("Build failure for {0}".format(i))
+#    build_fail += 1
+print(build_fail + run_ok + run_fail, "tests:")
+if build_fail > 0:
+  print("\t{0} failed to build".format(build_fail))
+if run_ok > 0:
+  print("\t{0} ran correctly".format(run_ok))
+if run_fail > 0:
+  print("\t{0} failed".format(run_fail))
+if timed_out_fail > 0:
+  print("\t{0} timed out".format(timed_out_fail))
+
+print('Failed:')
+for t in fails:
+  print(t)
+print('Timed out:')
+for t in timed_out:
+  print(t)
+
+if len(sys.argv) == 1:
+  try:
+    input(".. completed - press ENTER")
+  except:
+    pass
+else:
+  sys.exit(build_fail + run_fail)
